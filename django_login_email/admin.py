c4a8053abc3e6451c7ee9617ef06f738803e8ea1
svtter <svtter@qq.com>
2025-01-14 20:48:32 +0800

    feat, add admin

--- django_login_email/admin.py
+++ django_login_email/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from django_login_email import models
+
 # Register your models here.
+
+
+@admin.register(models.EmailRegister)
+class EmailRegisterAdmin(admin.ModelAdmin):
+  list_display = ("email", "expired_time", "validated")
+
+
+@admin.register(models.EmailLogin)
+class EmailLoginAdmin(admin.ModelAdmin):
+  list_display = ("email", "expired_time", "validated")
